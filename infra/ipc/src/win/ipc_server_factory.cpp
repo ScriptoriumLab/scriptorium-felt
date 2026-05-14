db1d2802cb14399bf99479b438c2191fd8efe9ae
Edward NoaLand <edwardnoaland@gmail.com>
2026-05-14 22:56:50 +0100

    feat: provide infra ipc layer in common lib

---
+++ infra/ipc/src/win/ipc_server_factory.cpp
@@ -0,0 +1,16 @@
+#include "modian/common/infra/ipc/ipc_server_factory.h"
+
+#include <memory>
+
+#include "modian/common/infra/ipc/win/sync_named_pipe_server.h"
+#include "modian/common/infra/ipc/win/async_named_pipe_server.h"
+
+namespace modian::common::infra::ipc {
+    std::unique_ptr<core::ipc::isync_ipc_server<std::string, std::string>> ipc_server_factory::create_sync_ipc_server(const std::string& name) {
+        return std::make_unique<sync_named_pipe_server>(name);
+    }
+
+    std::unique_ptr<core::ipc::iasync_ipc_server<std::string, std::string>> ipc_server_factory::create_async_ipc_server(const std::string& name) {
+        return std::make_unique<async_named_pipe_server>(name);
+    }
+}
